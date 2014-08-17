80c5ada64d0316137bd8f0270e90fe6a8a0db111
frost <aaron.frost@domo.com>
2014-08-17 16:53:00 -0600

    Upate the create

--- sqls/_1-createmessages.sql
+++ sqls/_1-createmessages.sql
@@ -1,6 +1,8 @@
-CREATE TABLE `test`.`messages` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NULL,
-  `message` VARCHAR(1000) NULL,
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `message` varchar(1000) DEFAULT NULL,
+  `created` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
